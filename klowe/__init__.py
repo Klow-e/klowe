135cfb65227af62b0cc933198b2f1e969d408fbf
Klow-e <chloevampi01@gmail.com>
2025-03-22 11:58:00 +0100

    housekeeping

--- klowe/__init__.py
+++ klowe/__init__.py
@@ -8,3 +8,9 @@ from .chisquare import *
 from .webscraper import *
 from .vectorspacemodel import *
 from .mathstuff import *
+
+
+# ToDo:
+# use TF_IDF to define genre
+# def func pa unir listas de listas
+# def func pa hacer los valores más extremos
