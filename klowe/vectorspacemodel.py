b5685145db1e7eae1255c78a19c5f8b8f338ce77
Klow-e <chloevampi01@gmail.com>
2025-03-23 12:31:47 +0100

    housekeeping

--- klowe/vectorspacemodel.py
+++ klowe/vectorspacemodel.py
@@ -118,13 +118,6 @@ def Kweight_model(text: str) -> dict[str, float]:
 # print(Kweight_model(wiki_article("Bacilo")))
 
 
-
-
-
-
-
-
-
 def define_genre(l_dicts: list[dict[str,float]]) -> dict[str,float]:
     all_keys: set[str] = {k for d in l_dicts for k in d}
     total_d = len(l_dicts)
