5357905eece89034e834465527230ebf92ffbeb3
Klow-e <chloevampi01@gmail.com>
2025-04-04 07:51:19 +0200

    housekeeping

--- klowe/__init__.py
+++ klowe/__init__.py
@@ -13,6 +13,12 @@ from .pythontools import *
 from .example_gloss import *
 
 
+# Clean Code Principles
+# # The code should explain itself
+# # A function should do one thing only
+# # A function does one thing if it cannot be extracted another meaningful function from it
+
+
 # ToDo:
 # setlang exclude chars
 # capitalize textprocessor functions
