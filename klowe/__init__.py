19e97a177a4caccca191955946cd46a83b1c304b
Klow-e <chloevampi01@gmail.com>
2025-04-06 16:02:34 +0200

    housekeeping

--- klowe/__init__.py
+++ klowe/__init__.py
@@ -12,27 +12,3 @@ from .datavisualization import *
 from .pythontools import *
 from .example_gloss import *
 
-
-# Clean Code Principles
-# # The code should explain itself
-# # The code should be beautiful and regular
-# # The code should be simple and easy
-# # The code should be explicit and flat
-# # The code should be defensive and errors be managed
-# # A function should only do one thing only
-# # One thing is when no other meaningful function can be extracted
-# # If the implementation is hard to explain, it's a bad idea
-
-
-# ToDo:
-# # setlang exclude chars
-# # capitalize textprocessor functions
-# # comment with ''' ''' function descriptions
-# # add chi^2 to theory
-# # create hebrew & greek parser
-
-# create bootcat clone
-# # Tuple generator
-# # lang detection with stopwords and html
-# # 
-
--- setup.py
+++ setup.py
@@ -34,3 +34,26 @@ setup(
     ],
 )
 
+
+# Clean Code Principles
+# # The code should explain itself
+# # The code should be beautiful and regular
+# # The code should be simple and easy
+# # The code should be explicit and flat
+# # The code should be defensive and errors be managed
+# # A function should only do one thing only
+# # One thing is when no other meaningful function can be extracted
+# # If the implementation is hard to explain, it's a bad idea
+
+
+# ToDo:
+# # capitalize textprocessor functions
+# # comment with ''' ''' function descriptions
+# # add chi^2 to theory
+# # create hebrew & greek parser
+
+# create bootcat clone
+# # Tuple generator
+# # lang detection with stopwords and html
+# # 
+
