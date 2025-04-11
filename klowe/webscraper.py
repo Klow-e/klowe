608ae6c1ae3bbcc3654cac24377056817a023cbb
Klow-e <chloevampi01@gmail.com>
2025-04-11 09:37:35 +0200

    search_engine added

--- klowe/webscraper.py
+++ klowe/webscraper.py
@@ -56,6 +56,22 @@ def WikiArticle(title: str) -> str:
 ###############################################################################################
 
 
+def search_engine(url: str) -> list[str]:
+    lang_code = {"en":"en,en", "es":"es-ES,es", "it":"it,it", "fr":"fr,fr"}.get("".join(KLanguage))
+    headers = {'Accept-Language': lang_code, 'Accept' : '*/*', 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.3.1 Safari/605.1.15',}
+    response = requests.get(url, headers = headers)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        links: list[str] = [str(i.get('href')) for i in soup.find_all('a')]
+        return links
+    else:
+        print(f"Error: {response.status_code} in {url}")
+        return [url, ]
+
+
+###############################################################################################
+
+
 def CleanTextFile(text: str) -> list[str]:
     text_l: list[str] = [i for i in text.split('\n')]
     # text_l: list[str] = [i for i in text_l if len(i) > 3]
